c938277673bfdeb810e53368b1361ab0337de342
rocedar <pruduct@rocedar.com>
2018-07-31 16:10:19 +0800

    0.2.2

---
+++ RCFamilyDoctor/Classes/RCFamilyDoctor.framework/Versions/A/Headers/RCMingYiPerfectHistoryInfoController.h
@@ -0,0 +1,13 @@
+//
+//  RCMingYiPerfectHistoryInfoController.h
+//  AFNetworking
+//
+//  Created by wtw on 2018/7/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RCOrderDetailModel;
+@interface RCMingYiPerfectHistoryInfoController : UIViewController
+@property (nonatomic,strong) RCOrderDetailModel *model;
+@end
---
+++ RCFamilyDoctor/Classes/RCFamilyDoctor.framework/Versions/A/Headers/RCPayWebViewController.h
@@ -0,0 +1,12 @@
+//
+//  RCPayWebViewController.h
+//  RCFamilyDoctor
+//
+//  Created by wtw on 2018/7/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCPayWebViewController : UIViewController
+@property(nonatomic,copy)NSString *urlStr;
+@end
